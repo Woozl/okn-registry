26c9455cc88a6c46ce2b849e32567261fe1bf9d4
David Glymph <Woozl@users.noreply.github.com>
2026-02-13 17:02:08 -0500

    fix workspace path

--- scripts/check_queries/index.js
+++ scripts/check_queries/index.js
@@ -3,11 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePaths = process.argv.slice(2);
+const workspace = process.argv[2];
+const filePaths = process.argv.slice(3);
 
 for (const filePath of filePaths) {
   try {
-    const content = fs.readFileSync(filePath, 'utf8');
+    const content = fs.readFileSync(path.join(workspace, filePath), 'utf8');
 
     console.log(content);
   } catch (error) {
